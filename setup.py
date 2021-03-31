46ce534e4c44d6a142d4e5708dd8e2f11b27e93e
DaTimeKeeper <zachwalsh2219@live.com>
2021-03-30 23:41:49 -0600

    update

--- setup.py
+++ setup.py
@@ -1,20 +1,29 @@
 import blynklib
-import time
+from time import sleep
+from picamera import PiCamera
 
-BLYNK_AUTH = 'wcQdKcosK1kE12NUpj9wBesbQh9i-ILY'
+BLYNK_AUTH = 'Y_3drVlwkx1gaWTjb54wCD4ERZSFBf2t'
+def capture():
+    camera = PiCamera()
+    camera.resolution = (1024, 768)
+    camera.start_preview()
+    # Camera warm-up time
+    sleep(2)
+    camera.capture('foo.jpg')
 
 blynk = blynklib.Blynk(BLYNK_AUTH)
 
 CONNECT_PRINT_MSG = '[CONNECT_EVENT]'
 DISCONNECT_PRINT_MSG = '[DISCONNECT_EVENT]'
 
-
 @blynk.handle_event("connect")
 def connect_handler():
     print(CONNECT_PRINT_MSG)
     print('Sleeping 2 sec in connect handler...')
     time.sleep(2)
-    blynk.disconnect()
+    blynk.notify("I an connected")
+    capture()
+
 
 
 @blynk.handle_event("disconnect")
