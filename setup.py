f46da0a451b3d8fec048cdb646768ac91ca591a2
DaTimeKeeper <zachwalsh2219@live.com>
2021-04-01 21:38:29 -0600

    changed stuff

--- setup.py
+++ setup.py
@@ -1,14 +1,34 @@
 import blynklib
-from time import sleep
+import time
 from picamera import PiCamera
+import io
+import picamera
+# from http.server import HTTPServer, BaseHTTPRquestHandler
 
 BLYNK_AUTH = 'Y_3drVlwkx1gaWTjb54wCD4ERZSFBf2t'
+# class Serv(BaseHTTPRquestHandler):
+#     def do_GET(self):
+#         if self.path =='/'
+#             self.path = '/foo.jpg'
+#         try:
+#             file_to_open = open(self.path[1:]).read()
+#             self.send_response(200)
+#         except:
+#             file_to_open = "File not Found"
+#             self.send_response(404)
+#         self.end_headers()
+#         self.wfile.write(bytes(file_to_open, 'utf-8'))
+    
+# def createServer():
+#     httpd = HTTPServer(('73.181.66.154', 8080), Serv)
+#     httpd.serve_forever()
+
 def capture():
     camera = PiCamera()
     camera.resolution = (1024, 768)
     camera.start_preview()
     # Camera warm-up time
-    sleep(2)
+    time.sleep(2)
     camera.capture('foo.jpg')
 
 blynk = blynklib.Blynk(BLYNK_AUTH)
@@ -23,6 +43,8 @@ def connect_handler():
     time.sleep(2)
     blynk.notify("I an connected")
     capture()
+    # createServer()
+    blynk.disconnect()
 
 
 
