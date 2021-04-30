017a6aa09b1874a68d8770d78e8b48bd80430bdf
DaTimeKeeper <zachwalsh2219@live.com>
2021-04-30 16:25:01 -0600

    Adds bash scrpit to make both python programs run

--- setup.py
+++ setup.py
@@ -2,129 +2,34 @@ import blynklib
 import time
 import io
 import picamera
-import logging
-import socketserver
-from threading import Condition
-from http import server
-#https://picamera.readthedocs.io/en/latest/recipes2.html#web-streaming
-
-PAGE="""\
-<html>
-<head>
-<title>Rasberry Pi Video Doorbell</title>
-</head>
-<body>
-<h1>PiCamera MJPEG Streaming Demo</h1>
-<img src="stream.mjpg" width="640" height="480" />
-</body>
-</html>
-"""
-
-class StreamingOutput(object):
-    def __init__(self):
-        self.frame = None
-        self.buffer = io.BytesIO()
-        self.condition = Condition()
-
-    def write(self, buf):
-        if buf.startswith(b'\xff\xd8'):
-            # New frame, copy the existing buffer's content and notify all
-            # clients it's available
-            self.buffer.truncate()
-            with self.condition:
-                self.frame = self.buffer.getvalue()
-                self.condition.notify_all()
-            self.buffer.seek(0)
-        return self.buffer.write(buf)
-
-class StreamingHandler(server.BaseHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == '/':
-            self.send_response(301)
-            self.send_header('Location', '/index.html')
-            self.end_headers()
-        elif self.path == '/index.html':
-            content = PAGE.encode('utf-8')
-            self.send_response(200)
-            self.send_header('Content-Type', 'text/html')
-            self.send_header('Content-Length', len(content))
-            self.end_headers()
-            self.wfile.write(content)
-        elif self.path == '/stream.mjpg':
-            self.send_response(200)
-            self.send_header('Age', 0)
-            self.send_header('Cache-Control', 'no-cache, private')
-            self.send_header('Pragma', 'no-cache')
-            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
-            self.end_headers()
-            try:
-                while True:
-                    with output.condition:
-                        output.condition.wait()
-                        frame = output.frame
-                    self.wfile.write(b'--FRAME\r\n')
-                    self.send_header('Content-Type', 'image/jpeg')
-                    self.send_header('Content-Length', len(frame))
-                    self.end_headers()
-                    self.wfile.write(frame)
-                    self.wfile.write(b'\r\n')
-            except Exception as e:
-                logging.warning(
-                    'Removed streaming client %s: %s',
-                    self.client_address, str(e))
-        else:
-            self.send_error(404)
-            self.end_headers()
-
-class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
-    allow_reuse_address = True
-    daemon_threads = True
-
-
 
 BLYNK_AUTH = 'Y_3drVlwkx1gaWTjb54wCD4ERZSFBf2t'
-
-def capture():
-
-    with picamera.PiCamera(resolution='640x480', framerate=24) as camera:
-        output = StreamingOutput()
-        camera.start_recording(output, format='mjpeg')
-        try:
-            address = ('', 8000)
-            server = StreamingServer(address, StreamingHandler)
-            server.serve_forever()
-        finally:
-            camera.stop_recording()
-
-
-    # camera = PiCamera()
-    # camera.resolution = (1024, 768)
-    # camera.start_preview()
-    # # Camera warm-up time
-    # time.sleep(2)
-    # camera.capture('foo.jpg')
 
 blynk = blynklib.Blynk(BLYNK_AUTH)
 
 CONNECT_PRINT_MSG = '[CONNECT_EVENT]'
 DISCONNECT_PRINT_MSG = '[DISCONNECT_EVENT]'
+WRITE_EVENT_PRINT_MSG = "[WRITE_VIRTUAL_PIN_EVENT] Pin: V{} Value: '{}'"
+READ_EVENT_PRINT_MSG = "[READ_EVENT_PRINT_MSG] Pin: V{} Value {}"
 
 @blynk.handle_event("connect")
 def connect_handler():
     print(CONNECT_PRINT_MSG)
-    print("Attempt Capture")
     #capture()
-    # blynk.disconnect()
-
-
-
+    
 @blynk.handle_event("disconnect")
 def disconnect_handler():
     print(DISCONNECT_PRINT_MSG)
     print('Sleeping 4 sec in disconnect handler...')
     time.sleep(4)
 
+@blynk.handle_event('write V1')
+def write_virtual_pin_handler(pin, value):
+    print(WRITE_EVENT_PRINT_MSG.format(pin,value))
+    if(value != 0):
+        blynk.notify("Someone is at the Door!")
 
+    
 ###########################################################
 # infinite loop that waits for event
 ###########################################################
